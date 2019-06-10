d52a63da7edf6a48237dd179152e149377f7d878
Martin McDaid <martin.mcdaid.etb@gmail.com>
2019-06-10 14:29:46 +0100

    Added the Contact model

---
+++ Photo1/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Photo1.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public bool ContactMe { get; set; }
+    }
+}
