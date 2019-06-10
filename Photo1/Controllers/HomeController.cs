5f5a3d273b1aef39df30349bfefa174887c725f3
Martin McDaid <martin.mcdaid.etb@gmail.com>
2019-06-10 11:56:29 +0100

    Added Gallery Page

--- Photo1/Controllers/HomeController.cs
+++ Photo1/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Photo1.Controllers
             return View();
         }
 
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
