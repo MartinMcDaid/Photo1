e6e78c6f05dfc5bfd6ce7356aca2b10519ba062b
Martin McDaid <martin.mcdaid.etb@gmail.com>
2019-06-11 11:49:53 +0100

    User Must be logged in to contact, also changed gallery to thumbnail

--- Photo1/Controllers/ContactController.cs
+++ Photo1/Controllers/ContactController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Photo1.Models;
 
 namespace Photo1.Controllers
 {
+    [Authorize]
     public class ContactController : Controller
     {
         private readonly IContactRepository _contactRepository;
