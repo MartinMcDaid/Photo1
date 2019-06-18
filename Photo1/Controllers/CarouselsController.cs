96dd2b14cd69c5415270a630cf76c34d1bd6850b
Martin McDaid <martin.mcdaid.etb@gmail.com>
2019-06-18 14:58:42 +0100

    added styling to view details, and some of the about page

--- Photo1/Controllers/CarouselsController.cs
+++ Photo1/Controllers/CarouselsController.cs
@@ -12,7 +12,6 @@ namespace Photo1.Controllers
     public class CarouselsController : Controller
     {
         private readonly AppDbContext _context;
-        private readonly ICarouselRepository _carouselRepository;
 
         public CarouselsController(AppDbContext context)
         {
@@ -44,26 +43,26 @@ namespace Photo1.Controllers
         }
 
         // GET: Carousels/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
         // POST: Carousels/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CarouselId,CarouselUrl")] Carousel carousel)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(carousel);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(carousel);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("CarouselId,CarouselUrl")] Carousel carousel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(carousel);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(carousel);
+        //}
 
         // GET: Carousels/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -117,33 +116,33 @@ namespace Photo1.Controllers
         }
 
         // GET: Carousels/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var carousel = await _context.Carousels
-                .FirstOrDefaultAsync(m => m.CarouselId == id);
-            if (carousel == null)
-            {
-                return NotFound();
-            }
+        //    var carousel = await _context.Carousels
+        //        .FirstOrDefaultAsync(m => m.CarouselId == id);
+        //    if (carousel == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(carousel);
-        }
+        //    return View(carousel);
+        //}
 
         // POST: Carousels/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var carousel = await _context.Carousels.FindAsync(id);
-            _context.Carousels.Remove(carousel);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var carousel = await _context.Carousels.FindAsync(id);
+        //    _context.Carousels.Remove(carousel);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
         private bool CarouselExists(int id)
         {
