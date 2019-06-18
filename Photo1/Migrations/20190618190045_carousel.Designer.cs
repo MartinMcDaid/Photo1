844bb44a5ffc44e3be1b04b6f33f95a36e3b5c3d
marty.mcdaid@gmail.com <marty.mcdaid@gmail.com>
2019-06-18 23:29:11 +0100

    added some css to the about page and gallery page, media queries

--- Photo1/Migrations/20190618190045_carousel.Designer.cs
+++ Photo1/Migrations/20190618190045_carousel.Designer.cs
@@ -10,14 +10,14 @@ using Photo1.Models;
 namespace Photo1.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20190618100442_carousel")]
+    [Migration("20190618190045_carousel")]
     partial class carousel
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.3-servicing-35854")
+                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
